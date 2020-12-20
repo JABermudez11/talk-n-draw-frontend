8a2fb220c835f829a2be487f401b85e3846b0387
JABermudez11 <justinanbermudez@gmail.com>
2020-12-19 19:01:41 -0800

    cleaned up horizontal navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -66,45 +66,4 @@ export const NavigationBar = (props) => {
   )
 }
 
-
-/*
-class NavigationBar extends Component {
-    render() {
-        const loggedIn = !!this.props.currentUser.id;
-        
-        return (
-                                               
-            <div className="navBar">
-                <NavLink to="/profile" >Profile</NavLink>
-                <NavLink to="/messages" >Messages</NavLink>
-                <NavLink to="/friends" >Friends</NavLink>
-                <NavLink to="/canvas" >Canvas</NavLink>
-
-                {loggedIn ? (
-                    <div className="item">
-                        {`Welcome ${this.props.currentUser.username}`}
-                    </div>
-                ) : null}
-                {loggedIn ? (
-                    <a
-                        onClick={() => {
-                            this.props.history.push('/login');
-                            this.props.handleLogout();
-                        }}
-                        className="item"
-                    >
-                        <button className="ui primary button">Log Out</button>
-                    </a>
-                ) : (
-                <Link to="/login" className="item">
-                    <div className="ui primary button">Log In</div>
-                </Link>
-                )}
-                <NavLink to="/register">New Account</NavLink>                
-            </div>
-                        
-        )
-    }
-}
-*/
 export default withRouter(NavigationBar)
