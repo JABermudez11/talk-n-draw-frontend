531c8994b085a07eea673d37d46563139380ce14
JABermudez11 <justinanbermudez@gmail.com>
2020-12-19 18:57:30 -0800

    refactoring sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
+import { StyledSideNav } from '../css/Style';
 import { BrowserRouter as Router, Route, Link, withRouter } from "react-router-dom";
 
-const StyledSideNav = styled.div`   
-    position: fixed;     /* Fixed Sidebar (stay in place on scroll and position relative to viewport) */
-    height: 100%;
-    width: 75px;     /* Set the width of the sidebar */
-    z-index: 1;      /* Stay on top of everything */
-    top: 3.4em;      /* Stay at the top */
-    background-color: #71ECCE; /* Black */
-    overflow-x: hidden;     /* Disable horizontal scroll */
-    padding-top: 10px;
-`;
 
 class SideNav extends React.Component {
     constructor(props) {
